492cbc0d8c69c5bba5b2a838e7a6720acce003a8
Ashutosh Solanki <ashutosh.solanki@check24.de>
2024-06-10 18:41:31 +0200

    Script for Linux Cluster

---
+++ script_cluster.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This script loads the necessary modules for the cluster
+
+# Load the cmake module
+module load cmake/3.21.4
+
+# Load the gcc module
+module load gcc/11.2.0
+
+# Load the boost module
+module load boost/1.75.0-gcc11
+
+# Load the xerces-c module
+module load xerces-c/3.2.1
