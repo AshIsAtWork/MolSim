1338662ea92c8187d228e853e2e2e166e76642c6
Ashutosh Solanki <ashutosh.solanki@check24.de>
2024-06-14 01:35:28 +0200

    Added helper function

--- Scripts/cluster_setup.sh
+++ Scripts/cluster_setup.sh
@@ -13,9 +13,33 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
+# Function to display help message
+function display_help() {
+    echo "Usage: $0 CLUSTER PARTITION MAIL_TYPE MAIL_USER CPUS_PER_TASK TIME"
+    echo
+    echo "Parameters:"
+    echo "  CLUSTER        Possible values: serial or inter"
+    echo "  PARTITION      Possible values: serial_std, serial_long or teramem_inter"
+    echo "  MAIL_TYPE      Possible values: BEGIN, END, FAIL, REQUEUE, ALL or NONE"
+    echo "  MAIL_USER      The email address to send the notifications. IMPORTANT: mail cannot be gmail, so use your university email"
+    echo "  CPUS_PER_TASK  The number of CPUs per task"
+    echo "  TIME           The time limit for the job in format \"HH:MM:SS\""
+    echo
+    echo "Example:"
+    echo "  $0 serial serial_std ALL your_university_email@example.com 4 02:00:00"
+    echo
+}
+
+# Check if the user requested help
+if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+    display_help
+    exit 0
+fi
+
 # Check if the number of parameters is correct
 if [ "$#" -ne 6 ]; then
     echo -e "${RED}Error: You need to provide exactly 6 parameters.${NC}"
+    echo -e "${RED}Run the script with -h or --help to see the usage.${NC}"
     exit 1
 fi
 
